44b8813c6e8f8868d1d8f9f2b4ffd79b80bd624e
ttmone <tomone.ameno@gmail.com>
2021-03-29 22:40:57 +0900

    header, footer, contactの不要部分の削除

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,12 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/g_nemo9',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
-  },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
-  },
-  {
-    Comp: Envelope,
-    alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'https://github.com/g-nemo9',
   },
 ]
 
@@ -37,16 +27,7 @@ export default function Contact() {
     <>
       <Header titlePre="Contact" />
       <div className={sharedStyles.layout}>
-        <div className={contactStyles.avatar}>
-          <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
-        </div>
-
         <h1 style={{ marginTop: 0 }}>Contact</h1>
-
-        <div className={contactStyles.name}>
-          JJ Kasper - Next.js Engineer @{' '}
-          <ExtLink href="https://vercel.com">Vercel</ExtLink>
-        </div>
 
         <div className={contactStyles.links}>
           {contacts.map(({ Comp, link, alt }) => {
